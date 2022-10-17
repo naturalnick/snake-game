862ca07bfdedce2f88d659a5f10e99064a012e57
Nick Schaefer <114598406+naturalnick@users.noreply.github.com>
2022-10-17 15:50:45 -0400

    start mapping out objects and functions

--- game.js
+++ game.js
@@ -1,2 +1,28 @@
 const gameCanvas = document.getElementById("game-canvas");
 const ctx = gameCanvas.getContext("2d");
+
+const snake = {
+    x: gameCanvas.width / 2,
+    y: gameCanvas.height / 2,
+    dirX: 2,
+    dirY: -2,
+    grow: function() {
+
+    }
+}
+
+const food = {
+    
+}
+
+function draw() {
+
+}
+
+function drawSnake() {
+
+}
+
+function drawFood() {
+
+}
