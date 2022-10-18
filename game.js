d4132f7055d91afe74d24893b0a88483ed2af0c6
Nick Schaefer <114598406+naturalnick@users.noreply.github.com>
2022-10-17 22:02:40 -0400

    minor edits, not much progress

--- game.js
+++ game.js
@@ -1,7 +1,8 @@
 const canvas = document.getElementById("game-canvas");
 const ctx = canvas.getContext("2d");
 
-let gameSpeed = 25;
+let gameSpeed = 30;
+let stopped = false;
 
 const snake = {
     x: canvas.width / 2,
@@ -10,37 +11,52 @@ const snake = {
     height: 10,
     dirX: 2,
     dirY: 0,
-    move: function() {
+    lastDirX: 0,
+    lastDirY: 0,
+    stopped: false,
+    move: function () {
         this.x += this.dirX;
         this.y += this.dirY;
     },
-    turnUp: function() {
+    turnUp: function () {
         if (this.dirY != 2) {
             this.dirX = 0;
             this.dirY = -2;
         }
     },
-    turnDown: function() {
+    turnDown: function () {
         if (this.dirY != -2) {
             this.dirX = 0;
             this.dirY = 2;
         }
     },
-    turnRight: function() {
+    turnRight: function () {
         if (this.dirX != -2) {
             this.dirX = 2;
             this.dirY = 0;
         }
     },
-    turnLeft: function() {
+    turnLeft: function () {
         if (this.dirX != 2) {
             this.dirX = -2;
             this.dirY = 0;
         }
     },
-    grow: function() {
-        this.width += 10;
+    stop: function () {
+        this.lastDirX = this.dirX;
+        this.lastDirY = this.dirY;
+        this.dirX = 0;
+        this.dirY = 0;
+        stopped = true;
     }
+    // grow: function () {
+
+    // },
+    // tail: {
+    //     width: globalThis.width,
+    //     height: globalThis.height,
+
+    // }
 }
 
 const food = {
@@ -48,7 +64,7 @@ const food = {
     y: 50,
     width: 10,
     height: 10,
-    reset: function() {
+    reset: function () {
         x = Math.floor(Math.random() * canvas.width - food.width);
         y = Math.floor(Math.random() * canvas.width - food.width);
     }
@@ -71,38 +87,44 @@ function drawFood() {
 }
 
 function checkBounds() {
-    if (snake.x + snake.dirX > canvas.width - snake.width + 2) return false; //what is 2?
-    if (snake.x + snake.dirX < snake.width) return false;
-    if (snake.y + snake.dirY > canvas.height - snake.height + 2) return false;
-    if (snake.y + snake.dirY < snake.height) return false;
-}
+    // console.log(snake.x + " " + (snake.y))
+    // console.log(snake.x > food.x)
+    if (snake.x + snake.width > canvas.width || snake.x < 0) endGame();
+    if (snake.y + snake.height > canvas.height || snake.y < 0) endGame();
+    if ((snake.x - food.width > food.x && snake.x < food.x + food.width) && (snake.y - food.height > food.y && snake.y < food.y + food.height)) {
+        snake.stop();
+    }
 
+
+}
+//snake.x + snake.dirX < food.x + food.width
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawSnake();
     drawFood();
-
-    if (checkBounds() === false) {
-        alert("GAME OVER");
-            document.location.reload();
-            clearInterval(interval);
-    }
+    checkBounds();
     snake.move();
 }
 
-const interval = setInterval(draw, 30);
+function endGame() {
+    alert("GAME OVER");
+    document.location.reload();
+    clearInterval(interval);
+}
+
+const interval = setInterval(draw, gameSpeed);
 
 document.addEventListener("keydown", keyDownHandler, false);
 
 function keyDownHandler(event) {
+    console.log(event.key)
     if (event.key === "Right" || event.key === "ArrowRight") {
         snake.turnRight();
-      } else if (event.key === "Left" || event.key === "ArrowLeft") {
+    } else if (event.key === "Left" || event.key === "ArrowLeft") {
         snake.turnLeft();
-      } else if (event.key === "Up" || event.key === "ArrowUp") {
+    } else if (event.key === "Up" || event.key === "ArrowUp") {
         snake.turnUp();
-      } else if (event.key === "Down" || event.key === "ArrowDown") {
+    } else if (event.key === "Down" || event.key === "ArrowDown") {
         snake.turnDown();
-      } 
-      
+    }
 }
