9b796bddebb7be73375a2ce27d185bc471b38b91
Nick Schaefer <114598406+naturalnick@users.noreply.github.com>
2022-10-18 20:30:22 -0400

    write readme, add gameover and restart capability

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# snake-game
+# Snake Game
+
+<br>
+
+_This is Snake, the game, made with Javascript and HTML canvas._
+
+<br>
+
+See it in action [here](https://naturalnick.github.io/snake-game/).
+
+<br>
+
+Screenshot:
+<image src="images/screenshot.png">
+
+## Summary
+
+Snake is a simple game involving a "snake" and an "apple".
+
+Use the arrow keys to navigate the canvas, but if you crash into the walls or yourself, you lose.
+
+Collect apples to gain length and points.
+
+This was a super fun game to make. I learned a whole lot about objects, HTML canvas, and calculating positioning.
+
+## Author
+
+- **Nick Schaefer** - _Full-Stack Software Developer_ - [Website](https://nschaefer.com/) | [LinkedIn](https://www.linkedin.com/in/nick-n-schaefer)
--- game.js
+++ game.js
@@ -6,103 +6,113 @@ const startingY = canvas.height / 2
 const gridSize = 10;
 
 let gameSpeed = 100;
-let stopped = false;
+let playing = false;
 let score = 0;
 
-const snake = {
-    width: 10,
-    length: 3,
-    dirX: 10,
-    dirY: 0,
-    bodyParts: [
-        {
-            x: startingX,
-            y: startingY,
+let interval;
+
+let snake = newSnake();
+let food = newFood();
+
+displayGameStatus();
+
+function newSnake() {
+    return {
+        width: 10,
+        dirX: 10,
+        dirY: 0,
+        bodyParts: [
+            {
+                x: startingX,
+                y: startingY,
+            },
+            {
+                x: startingX - this.width,
+                y: startingY,
+            },
+            {
+                x: startingX - (this.width * 2),
+                y: startingY,
+            }
+        ],
+        move: function () {
+            for (let n = this.bodyParts.length - 1; n > 0; n--) {
+                this.bodyParts[n].x = this.bodyParts[n - 1].x
+                this.bodyParts[n].y = this.bodyParts[n - 1].y
+            }
+            this.bodyParts[0].x += this.dirX;
+            this.bodyParts[0].y += this.dirY;
         },
-        {
-            x: startingX - this.width,
-            y: startingY,
+        turnUp: function () {
+            if (this.dirY != 10) {
+                this.dirX = 0;
+                this.dirY = -10;
+            }
         },
-        {
-            x: startingX - (this.width * 2),
-            y: startingY,
-        }
-    ],
-    move: function () {
-        for (let n = this.bodyParts.length - 1; n > 0; n--) {
-            this.bodyParts[n].x = this.bodyParts[n - 1].x
-            this.bodyParts[n].y = this.bodyParts[n - 1].y
-        }
-        this.bodyParts[0].x += this.dirX;
-        this.bodyParts[0].y += this.dirY;
-    },
-    turnUp: function () {
-        if (this.dirY != 10) {
-            this.dirX = 0;
-            this.dirY = -10;
-        }
-    },
-    turnDown: function () {
-        if (this.dirY != -10) {
-            this.dirX = 0;
-            this.dirY = 10;
-        }
-    },
-    turnRight: function () {
-        if (this.dirX != -10) {
-            this.dirX = 10;
-            this.dirY = 0;
-        }
-    },
-    turnLeft: function () {
-        if (this.dirX != 10) {
-            this.dirX = -10;
-            this.dirY = 0;
-        }
-    },
-    grow: function () {
-        const tail = this.bodyParts[this.bodyParts.length - 1];
-        if (this.dirX != 0) {
-            this.bodyParts.push({x: tail.x + (this.dirX * -1), y: tail.y});
-        } else if (this.dirY != 0) {
-            this.bodyParts.push({x: tail.x, y: tail.y + (this.dirY * -1)});
+        turnDown: function () {
+            if (this.dirY != -10) {
+                this.dirX = 0;
+                this.dirY = 10;
+            }
+        },
+        turnRight: function () {
+            if (this.dirX != -10) {
+                this.dirX = 10;
+                this.dirY = 0;
+            }
+        },
+        turnLeft: function () {
+            if (this.dirX != 10) {
+                this.dirX = -10;
+                this.dirY = 0;
+            }
+        },
+        grow: function () {
+            const tail = this.bodyParts[this.bodyParts.length - 1];
+            if (this.dirX != 0) {
+                this.bodyParts.push({ x: tail.x + (this.dirX * -1), y: tail.y });
+            } else if (this.dirY != 0) {
+                this.bodyParts.push({ x: tail.x, y: tail.y + (this.dirY * -1) });
+            }
         }
     }
 }
 
-const food = {
-    x: 200,
-    y: 200,
-    width: 10,
-    eaten: false,
-    reset: function () {
-        this.x = generateGridPosition(food.width, canvas.width - food.width);
-        this.y = generateGridPosition(food.width, canvas.height - food.width);
-        this.eaten = false;
+function newFood(x, y) {
+    return {
+        width: 10,
+        x: x,
+        y: y,
+        eaten: false,
     }
 }
 
 function drawSnake() {
     ctx.beginPath();
-    console.log(snake.bodyParts)
     for (let n = 0; n < snake.bodyParts.length; n++) {
         ctx.rect(snake.bodyParts[n].x, snake.bodyParts[n].y, snake.width, snake.width);
     }
     ctx.fillStyle = "black";
     ctx.fill();
-    // ctx.closePath();
+    ctx.closePath();
 }
 
 function drawFood() {
     if (food.eaten) {
         ctx.clearRect(food.x, food.y, food.width, food.width);
-        food.reset();
+        generateNewFood();
     }
     ctx.beginPath();
     ctx.rect(food.x, food.y, food.width, food.width);
     ctx.fillStyle = "green";
     ctx.fill();
     ctx.closePath();
+}
+
+function generateNewFood() {
+    const foodX = getRandCoord(food.width, canvas.width - food.width);
+    const foodY = getRandCoord(food.width, canvas.height - food.width);
+    food = newFood(foodX, foodY);
 }
 
 function checkBounds() {
@@ -116,7 +126,8 @@ function checkBounds() {
         (headY > food.y - food.width && headY < food.y + food.width)) {
         snake.grow();
         food.eaten = true;
-        updateScore();
+        score++;
+        displayScore();
     }
     //snake hits self
     for (let n = 1; n < snake.bodyParts.length; n++) {
@@ -136,14 +147,19 @@ function playGame() {
 }
 
 function endGame() {
-    alert("GAME OVER");
-    document.location.reload();
     clearInterval(interval);
+    displayGameStatus();
+    playing = false;
 }
 
-const interval = setInterval(playGame, gameSpeed);
+function resetGame() {
+    snake = newSnake();
+    generateNewFood();
+    score = 0;
+    displayScore();
+}
 
-function generateGridPosition(min, max) {
+function getRandCoord(min, max) {
     const randomNum = Math.floor(Math.random() * (max - min + 1) + min);
     const roundedNum = Math.round(randomNum / gridSize) * gridSize;
     return roundedNum;
@@ -160,10 +176,29 @@ function keyDownHandler(event) {
         snake.turnUp();
     } else if (event.key === "Down" || event.key === "ArrowDown") {
         snake.turnDown();
+    } else if (event.key === " " || event.key === "Space") {
+        if (!playing) {
+            resetGame();
+            interval = setInterval(playGame, gameSpeed);
+            playing = true;
+        }
     }
 }
 
-function updateScore() {
-    score++;
+function displayGameStatus() {
+    const start = "Press the space key to begin.";
+    const lost = "GAME OVER";
+    ctx.font = "26px 'Gill Sans'";
+    ctx.textAlign = "center";
+    if (playing) {
+        ctx.fillStyle = "red";
+        ctx.fillText(lost, startingX, startingY);
+    } else {
+        ctx.fillStyle = "green";
+        ctx.fillText(start, startingX, startingY);
+    }
+}
+
+function displayScore() {
     document.getElementById("score").textContent = `Score: ${score}`;
 }
